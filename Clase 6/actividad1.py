6999be92be7588385c4bfd17ece14311137cff95
pridapablo <pridapablo@gmail.com>
2021-08-20 09:52:09 -0500

    main

--- Clase 6/actividad1.py
+++ Clase 6/actividad1.py
@@ -1,4 +1,13 @@
-def cadenas():
+# Ejercicio 1
+# El área y volumen de una esfera se calcula con las siguientes fórmulas:
+# área =  4𝜋𝑟2
+# volumen = 4𝜋𝑟33
+# Escribe un programa que pida el valor del radio y muestre su área y su volumen.
+
+def área():
     x = "dios"
+
+def volumen():
+    x = 10
 
 if __name__ == "__main__":
