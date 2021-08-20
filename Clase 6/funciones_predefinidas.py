243dc90ba7fd0af72b05aa946ee8e62bd6aa9f0a
pridapablo <pridapablo@gmail.com>
2021-08-20 09:40:53 -0500

    main

--- Clase 6/funciones_predefinidas.py
+++ Clase 6/funciones_predefinidas.py
@@ -9,10 +9,15 @@ def cadenas():
 
 def números():
     x = range(5)
-    #secuencia de números
+    # range hace un rango entre cero y 5
+    #secuencia de números hasta el anterior del número que pones entre paréntesis
     print(list(x))
     #list convierte de cadena de valores numéricos a una lista
+    y = [3,5,9]
+    print(max(y))
+    print(min(y))
+    #imprime el más chico o el más grande de una lista de números (guardados en la variable y)
 
 if __name__ == "__main__":
-    #cadenas()
+    cadenas()
     números()
