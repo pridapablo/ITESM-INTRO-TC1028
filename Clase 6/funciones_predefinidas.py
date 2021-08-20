2e871a4edd79f59ee2738a36be342e3545c82cb0
pridapablo <pridapablo@gmail.com>
2021-08-20 09:26:00 -0500

    clase 5

--- Clase 6/funciones_predefinidas.py
+++ Clase 6/funciones_predefinidas.py
@@ -1,7 +1,11 @@
 def cadenas():
     print("Hola")
+    #imprimir
     print(len("Hola mundo"))
+    #te da un entero con la cantidad de caracteres de la cadena
     a = ("Hola esto va a ser una lista")
+    print(a.split())
+    #split separa la cadena de caracteres usando los espacios (la separa en palabras diferentes)
 
 if __name__ == "__main__":
     cadenas()
