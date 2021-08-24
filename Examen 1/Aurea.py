897e8b02ebff9e450b581357fcbae84346b0cb05
pridapablo <pridapablo@gmail.com>
2021-08-24 09:20:34 -0500

    ex1

---
+++ Examen 1/Aurea.py
@@ -0,0 +1,14 @@
+import math as m
+
+def aurea():
+    no_para_multiplicar = float(input("Número: "))
+    decimales = int(input("Decimales a mostrar: "))
+    
+    varphi = (1 + m.sqrt(5))/2
+
+    resultado = varphi*no_para_multiplicar
+
+    print("Razón áurea: "+str(round(resultado,decimales)))
+
+if __name__ == "__main__":
+    aurea()
