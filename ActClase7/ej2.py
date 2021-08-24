4dce86b284d3eb3976e0ccf3627cb5e17e2e4610
pridapablo <pridapablo@gmail.com>
2021-08-24 10:43:20 -0500

    ej2

--- ActClase7/ej2.py
+++ ActClase7/ej2.py
@@ -3,8 +3,12 @@ Y = int(input("ingresa longitud 2 "))
 Z = int(input("ingresa longitud 3 "))
 
 if (X>0  and Y>0 and Z>0) and (X + Y > Z) and (X + Z > Y) and (Y + Z > X):
-    print("X, Y y Z son los lados de un triángulo")
+    if X == Y and X == Z:
+        print("el triángulo es equilatero")
+    elif  X == Y or X == Z or Y == Z:
+        print("el triángulo es isóceles")
+    else:
+        print("el triángulo es escaleno")
 else:
     print("X, Y y Z no son lados de un triángulo")
 
-    
