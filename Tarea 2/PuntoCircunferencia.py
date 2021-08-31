47b2b762eb59ef0c9c7cb98ee4bb5077bc50a5a8
pridapablo <pridapablo@gmail.com>
2021-08-30 23:17:23 -0500

    db

---
+++ Tarea 2/PuntoCircunferencia.py
@@ -0,0 +1,23 @@
+#punto circunferencia
+
+import math as m
+
+def pto():
+    radio = float(input("Introduce el radio: "))
+    x1 =  float(input("Introduce x1: "))
+    x2 =  float(input("Introduce x2: "))
+    y1 =  float(input("Introduce y1: "))
+    y2 =  float(input("Introduce y2: "))
+
+    if ():
+        return "DENTRO"
+
+    if ():
+        return "FUERA"
+
+    if ():
+        return "SOBRE"
+
+if __name__ == "__main__":
+    punto = pto()
+    print(punto)
