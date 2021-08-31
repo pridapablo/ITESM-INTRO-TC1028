6a3ddc075040a7c4f59eed726c5eb72a78af58b5
pridapablo <pridapablo@gmail.com>
2021-08-31 18:08:58 -0500

    f yeah

--- Tarea 2/DigitosPares (Opcional).py
+++ Tarea 2/DigitosPares (Opcional).py
@@ -1,1 +1,22 @@
-# Ver como usar matemáticas para moverse entre cifra y cifra
+# nota: cero es par
+
+def main():
+    entrada = int(input("Dame un número: "))
+    i = 0 # yo se que no hemos aprendido iteraciones, pero creo que es la forma más fácil de hacerlo
+    count = 0 # inicializo el iterador y el conteo en cero porque si no no corre la función
+
+    if (entrada < 9999) and (entrada > 999): # test 4 dígitos donde el primero no es cero
+
+        entrada_cadena = str(entrada) #convierto la entrada en cadena para poder recorrer sus índices
+
+        for i in range(0, 4): # lo hago 4 veces
+            if int(entrada_cadena[i]) % 2 == 0: # si el residuo es cero...
+                count = count + 1 # entonces sumo 1 a la cuenta
+    
+        print("El número de dígitos pares es: ", count) # imprimo la cuenta que llevo
+
+    else:
+        print("Por favor ingresa un número de 4 dígitos que no comience con cero") # mensaje de error
+
+if __name__ == '__main__':
+    main()
