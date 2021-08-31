a5b4547d35966599bb5d643d5c0a51cb8c88df0b
pridapablo <pridapablo@gmail.com>
2021-08-31 00:43:18 -0500

    añoooossss

--- Tarea 2/PiedraPapelTijera.py
+++ Tarea 2/PiedraPapelTijera.py
@@ -1,20 +1,41 @@
 # piedra papel o tijera
 def ppot():
-    ana = input("Tirada de Ana: ")
-    juan = input("Tirada de Juan: ")
+    ana = str(input("Tirada de Ana: "))
+    juan = str(input("Tirada de Juan: "))
 
     #  piedra = "a"
     #  papel = "p"
     #  tijera = "t"
 
-    if ():
+    if len(ana) > 1:
+        return "Las tiradas deben de ser un caracter"
+
+    elif len(juan) > 1:
+        return "Las tiradas deben de ser un caracter" 
+        
+    elif (ana == "a" and juan == "t"):
+        return "Gana Ana"
+    
+    elif (ana == "t" and juan == "p"):
         return "Gana Ana"
 
-    if ():
+    elif (ana == "p" and juan == "a"):
+        return "Gana Ana"
+
+    elif (juan == "a" and ana == "t"):
+        return "Gana Juan"
+    
+    elif (juan == "t" and ana == "p"):
         return "Gana Juan"
 
-    if ():
-        return "Empate"
+    elif (juan == "p" and ana == "a"):
+        return "Gana Juan"
+
+    elif (ana == juan) and ana == ("a" or "p" or "t"):
+        return "Empate"   
+
+    else:
+        return "Tirada incorrecta"
 
 if __name__ == "__main__":
     ret = ppot()
