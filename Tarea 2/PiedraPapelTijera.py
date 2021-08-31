ecfbc64a531e94c71a2e0be20f90c9f121d75092
pridapablo <pridapablo@gmail.com>
2021-08-30 23:18:23 -0500

    no

---
+++ Tarea 2/PiedraPapelTijera.py
@@ -0,0 +1,21 @@
+# piedra papel o tijera
+def ppot():
+    ana = input("Tirada de Ana: ")
+    juan = input("Tirada de Juan: ")
+
+    #  piedra = "a"
+    #  papel = "p"
+    #  tijera = "t"
+
+    if ():
+        return "Gana Ana"
+
+    if ():
+        return "Gana Juan"
+
+    if ():
+        return "Empate"
+
+if __name__ == "__main__":
+    ret = ppot()
+    print(ret)
