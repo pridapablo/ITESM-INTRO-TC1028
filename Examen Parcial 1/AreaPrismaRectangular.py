5dffc66923a4da11e853b74719315942514f6a2c
pridapablo <pridapablo@gmail.com>
2021-09-10 09:54:47 -0500

    2

---
+++ Examen Parcial 1/AreaPrismaRectangular.py
@@ -0,0 +1,26 @@
+# Pablo Banzo Prida - A01782031
+
+def area(b,a):
+    res = b*a
+    return res
+
+def area_total():
+    area_prisma = area()
+    res = 
+    #dos extremos + 4 otros
+    #suma de las 6 caras en total
+    return 1
+
+def main():
+    print("Realizado por: ")
+    print("Pablo Banzo Prida - A01782031")
+    print("-----------------------------")
+
+    base = float(input("Dame la base: "))
+    altura = float(input("Dame la altura: "))
+    profundidad = float(input("Dame la profundidad: "))
+
+    print(area_total())
+
+if __name__ == '__main__':
+    main()
