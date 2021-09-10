4e382f68928c9c0b08b39695c70fb3cfb499970f
pridapablo <pridapablo@gmail.com>
2021-09-10 10:06:09 -0500

    commit

--- Examen Parcial 1/AreaPrismaRectangular.py
+++ Examen Parcial 1/AreaPrismaRectangular.py
@@ -4,12 +4,18 @@ def area(b,a):
     res = b*a
     return res
 
-def area_total():
-    area_prisma = area()
-    res = 
-    #dos extremos + 4 otros
-    #suma de las 6 caras en total
-    return 1
+def area_total(area,prof,b,a):
+    # res_1: dos extremos sin profundidad
+    res_1 = area*2
+    
+    # res_2 + res_3 = 4 caras con profundidad
+    res_2 = 2*(b*prof)
+    res_3 = 2*(a*prof)
+
+    #res = suma de las 6 caras en total
+    res = res_1 + res_2 + res_3
+
+    return res
 
 def main():
     print("Realizado por: ")
@@ -20,7 +26,7 @@ def main():
     altura = float(input("Dame la altura: "))
     profundidad = float(input("Dame la profundidad: "))
 
-    print(area_total())
+    print("El área total del prisma es:", area_total(area(base,altura),profundidad,base,altura))
 
 if __name__ == '__main__':
     main()
