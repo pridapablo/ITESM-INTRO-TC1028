a2431716821294c6c2dd88c483788df53abd0f45
pridapablo <pridapablo@gmail.com>
2021-10-01 10:32:35 -0500

    m

---
+++ ActClase18/CálculoDeIntereses.py
@@ -0,0 +1,22 @@
+import math as m
+
+def main():
+    inicial = float(input("Inserte la cantidad de dinero inicial: "))
+    porcentaje = int(input("Inserte el porcentaje: "))
+
+    intereses(inicial,porcentaje)
+
+def intereses(c,p):
+    if c > 0 and p > 0:
+        p_mensual_y_decimal = p/12/100
+
+        for i in range(12):
+            c = (1+p_mensual_y_decimal) * c
+
+        print(m.trunc(c))
+
+    else:
+        print("Error en los datos")
+
+if __name__ == "__main__":
+    main()
