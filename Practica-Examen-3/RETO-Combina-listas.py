248f0919cd6b6a4451442a06be47e9eea2c6165b
Pablo Banzo <89087453+pridapablo@users.noreply.github.com>
2021-10-12 14:56:21 +0000

    push

--- Practica-Examen-3/RETO-Combina-listas.py
+++ Practica-Examen-3/RETO-Combina-listas.py
@@ -1,5 +1,35 @@
 def main():
-  pass
+  n_L1 = int(input())
+  n_L2 = int(input())
+
+  if (n_L1 <= 0) or (n_L2 <= 0):
+    print("Error")
+    exit()
+  else:
+    print("-----")
+    L1 = []
+    for i in range(n_L1):
+      L1.append(input())
+
+    print("-----")
+    L2 = []
+    for i in range(n_L2):
+      L2.append(input())      
+
+  print("-----")
+
+  print(L1)
+  print(L2)
+
+  combo = []
+
+  for i in range(0,(len(L1)*2),2):
+    combo.append(L1[i])
+
+  for i in range(1,len(L2)*2,2):
+    combo.append(L2[i])
+
+  print(combo)
 
 if __name__ == "__main__":
   main()
--- Practica-Examen-3/RETO-Mezcla-ordena-listas.py
+++ Practica-Examen-3/RETO-Mezcla-ordena-listas.py
@@ -13,18 +13,35 @@ def main():
   del L2[len(L2)-1]
 
   for i in range(len(L1)):
-    lista.append(L1[i])
+    lista.append(int(L1[i]))
   for i in range(len(L2)):
-    lista.append(L2[i])
+    lista.append(int(L2[i]))
 
   lista.sort()
 
   print("L1=")
-  print(L1)
+  print("[",end="")
+  if len(L1) != 0:
+    for i in range(len(L1)-1):
+      print(str(L1[i]),end=", ")
+    print(L1[-1],end="")
+  print("]")
+  
   print("L2=")
-  print(L2)
+  print("[",end="")
+  if len(L2) != 0:
+    for i in range(len(L2)-1):
+      print(str(L2[i]),end=", ")
+    print(L2[-1],end="")
+  print("]")
+
   print("LORDENADA=")
-  print(lista)
+  print("[",end="")
+  if len(lista) != 0:
+    for i in range(len(lista)-1):
+      print(str(lista[i]),end=", ")
+    print(lista[-1],end="")
+  print("]")
 
 if __name__ == "__main__":
   main()
